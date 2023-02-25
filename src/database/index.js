b275e79335ba6b4a5e733744361de42bd50cb840
Jonathan <jonathangh2030@gmail.com>
2023-02-25 16:44:17 -0300

    feat(database): catching de errores

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,12 @@ const { connect } = require('mongoose');
 const { HOST_DB } = require('../config/environment');
 
 const connectDB = async () => {
-  const { connection } = await connect(HOST_DB);
-  console.log(`connected to the database: ${connection.name}`);
+  try {
+    const { connection } = await connect(HOST_DB);
+    console.log(`connected to the database: ${connection.name}`);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDB;
