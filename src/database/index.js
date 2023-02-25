9a6f2113562bf4983bae79184899b858e4aa0f16
Jonathan <jonathangh2030@gmail.com>
2023-02-25 16:52:48 -0300

    fix(database): deprecation warning

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
-const { connect } = require('mongoose');
+const { connect, set } = require('mongoose');
 
 const { HOST_DB } = require('../config/environment');
 
 const connectDB = async () => {
   try {
+    set('strictQuery', false);
     const { connection } = await connect(HOST_DB);
     console.log(`connected to the database: ${connection.name}`);
   } catch (error) {
