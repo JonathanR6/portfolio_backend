171824bd83d4783adb3b66d3493080eeea3af00b
Jonathan <jonathangh2030@gmail.com>
2023-02-27 15:58:14 -0300

    feat(api/middlewaers): configuracion de middlewares para subir archivos

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 const middlewares = express.Router();
 
 middlewares
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
   .use(cors({ origin: '*' }))
   .use(morgan('dev'))
-  .use(express.json());
+  .use(fileUpload());
 
 module.exports = middlewares;
