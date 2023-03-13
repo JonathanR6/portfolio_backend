8a99495e63960e2941478d5393b4c300bf9cc8b2
Jonathan <jonathangh2030@gmail.com>
2023-03-12 22:56:07 -0300

    feat(middlewares): limitacion de peso de los archivos

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -6,10 +6,10 @@ const cors = require('cors');
 const middlewares = express.Router();
 
 middlewares
-  .use(express.urlencoded({ extended: true }))
-  .use(express.json())
+  .use(express.urlencoded({ extended: true, limit: 50 * 1024 }))
+  .use(express.json({ limit: 50 * 1024 }))
   .use(cors({ origin: '*' }))
   .use(morgan('dev'))
-  .use(fileUpload());
+  .use(fileUpload({ limits: { fileSize: 50 * 1024 }, abortOnLimit: true }));
 
 module.exports = middlewares;
